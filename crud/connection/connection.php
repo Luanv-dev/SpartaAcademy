c4386fcb55e9813d2a9bf23036ec5b9fd518d774
Luan Victor Leite da Silva <luan.silva@me.com.br>
2024-10-26 23:11:52 -0300

    "spartaacademy"

--- crud/connection/connection.php
+++ crud/connection/connection.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "SpartaAcademy";
+    $dbname = "spartaacademy";
 
     $conexao = new mysqli($servername, $username, $password, $dbname);
     if ($conexao->connect_error) {
