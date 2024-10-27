0b1ec796617e0c77b52d752cea77901fd3208fe2
Luan Victor Leite da Silva <luan.silva@me.com.br>
2024-10-26 23:09:48 -0300

    ajuste

--- crud/register/registerClient.php
+++ crud/register/registerClient.php
@@ -5,15 +5,18 @@ if(isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
 
+    // Proteção contra SQL Injection
+    $name = mysqli_real_escape_string($connection, $name);
+    $email = mysqli_real_escape_string($connection, $email);
+
     $query = "INSERT INTO register (nome, email) 
               VALUES ('$name','$email')";
 
     $result = mysqli_query($connection, $query);
 
     if($result) {
-        header('Location: listagem.php');
+        echo "Cadastro realizado com sucesso!";
     } else {
         echo "Erro ao cadastrar: " . mysqli_error($connection);
     }
 }
-?>
